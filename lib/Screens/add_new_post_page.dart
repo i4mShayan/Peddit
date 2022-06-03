c0df5648cbc72dbc9c0b999956fe25beb8582c4d
Shayan Kebriti <shayankebriti@gmail.com>
2022-06-03 15:50:10 +0430

    floating button bar added

--- lib/Screens/add_new_post_page.dart
+++ lib/Screens/add_new_post_page.dart
@@ -32,122 +32,157 @@ class _AddNewPostPageState extends State<AddNewPostPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-          Container(
-            margin: EdgeInsets.only(left: 20 , right: 20 , bottom: 15),
-            child: TextFormField(
-              onChanged: (value){
-                setState(() {
-                  canCreatePost = (_postText.text.isNotEmpty && _postTitle.text.isNotEmpty);
-                });
-              },
-              controller: _postTitle,
-              style: TextStyle(
-                fontSize: 30
-              ),
-              cursorColor: Colors.grey,
-              decoration: InputDecoration(
-                // hintStyle: TextStyle(
-                //   fontSize: 30,
-                // ),
-                border: InputBorder.none,
-                hintText: "An interesting title",
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(right: 20 , left: 20),
-            child: TextFormField(
-              onChanged: (value){
-                setState(() {
-                  canCreatePost = (_postText.text.isNotEmpty && _postTitle.text.isNotEmpty);
-                });
-              },
-              controller: _postText,
-              maxLines: 20,
-              cursorColor: Colors.grey[600],
-              style: TextStyle(
-                  fontSize: 20
-              ),
-              decoration: InputDecoration(
-                border: InputBorder.none /*OutlineInputBorder()*/,
-                hintText: "Your text post",
-              ),
-            ),
-          ),
-          SizedBox(height: 70,),
-        ]),
-      ),
-      floatingActionButton: Container(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Flexible(
-              child: Container(
-                // width: MediaQuery.of(context).size.width,
-                // margin: const EdgeInsets.only(left: 20, right: 20,),
-                height: 40,
-                decoration: const ShapeDecoration(
-                  shape: StadiumBorder(),
-                  gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: [Color(0xff374ABE), Colors.blue],
-                  ),
-                ),
-                child: MaterialButton(
-                  onPressed: () => pickImage(),
-                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  shape: const StadiumBorder(),
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          body: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 5),
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Icon(Icons.photo_library_rounded, color: Colors.white,),
-                      SizedBox(width: 10,),
-                      Text(
-                        'Select image',
-                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      IconButton(onPressed: (){Navigator.pop(context);}, icon: Icon(Icons.close_rounded, size: 30,)),
+                      Container(
+                        width: 80,
+                        margin: const EdgeInsets.only(right: 5,),
+                        height: 40,
+                        decoration: ShapeDecoration(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50),
+                          ),
+                          gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: (canCreatePost ? [Color(0xff374ABE), Colors.blue]:[Color(0xff374ABE).withOpacity(0.6), Colors.blue.withOpacity(0.6)]),
+                          ),
+                        ),
+                        child: MaterialButton(
+                          splashColor: canCreatePost ? null:Colors.transparent,
+                          highlightColor: canCreatePost ? null:Colors.transparent,
+                          onPressed: () {},
+                          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50),
+                          ),
+                          child: const Text(
+                            'Next',
+                            style: TextStyle(color: Colors.white, fontSize: 17,fontWeight: FontWeight.w500),
+                          ),
+                        ),
                       ),
                     ],
                   ),
                 ),
-              ),
-            ),
-            Flexible(
-              child: Container(
-                width: MediaQuery.of(context).size.width/3,
-                // margin: const EdgeInsets.only(left: 20, right: 20,),
-                height: 40,
-                decoration: ShapeDecoration(
-                  shape: StadiumBorder(),
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: (canCreatePost ? [Colors.red, Colors.orange]:[Colors.red.withOpacity(0.6), Colors.orange.withOpacity(0.6)]),
+                Container(
+                  margin: EdgeInsets.only(left: 20 , right: 20 , bottom: 5),
+                  child: TextFormField(
+                    onChanged: (value){
+                      setState(() {
+                        canCreatePost = (_postText.text.isNotEmpty && _postTitle.text.isNotEmpty);
+                      });
+                    },
+                    controller: _postTitle,
+                    style: TextStyle(
+                      fontSize: 30
+                    ),
+                    cursorColor: Colors.grey,
+                    decoration: InputDecoration(
+                      // hintStyle: TextStyle(
+                      //   fontSize: 30,
+                      // ),
+                      border: InputBorder.none,
+                      hintText: "An interesting title",
+                    ),
                   ),
                 ),
-                child: MaterialButton(
-                  splashColor: canCreatePost ? null:Colors.transparent,
-                  highlightColor: canCreatePost ? null:Colors.transparent,
-                  onPressed: () {},
-                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  shape: const StadiumBorder(),
-                  child: const Text(
-                    'Post',
-                    style: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
+                Container(
+                  margin: EdgeInsets.only(right: 20 , left: 20),
+                  child: TextFormField(
+                    onChanged: (value){
+                      setState(() {
+                        canCreatePost = (_postText.text.isNotEmpty && _postTitle.text.isNotEmpty);
+                      });
+                    },
+                    controller: _postText,
+                    maxLines: 20,
+                    cursorColor: Colors.grey[600],
+                    style: TextStyle(
+                        fontSize: 20
+                    ),
+                    decoration: InputDecoration(
+                      border: InputBorder.none /*OutlineInputBorder()*/,
+                      hintText: "Your text post",
+                    ),
                   ),
                 ),
-              ),
+                SizedBox(height: 70,),
+              ]),
             ),
-          ],
+          floatingActionButton: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              ButtonBar(
+                children: [
+                  IconButton(
+                      onPressed: (){},
+                      icon: Icon(Icons.photo_outlined,)
+                  ),
+                  IconButton(
+                      onPressed: (){},
+                      icon: Icon(Icons.photo_camera_outlined,)
+                  ),
+                ],
+              ),
+            ],
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+          // bottomNavigationBar: Container(
+          //   decoration: const BoxDecoration(
+          //     boxShadow: <BoxShadow>[
+          //       BoxShadow(
+          //           color: Colors.black54,
+          //           blurRadius: 5.0,
+          //           offset: Offset(0.0, 0.75)
+          //       )
+          //     ],
+          //   ),
+          //   child: BottomNavigationBar(
+          //     elevation: 1,
+          //     iconSize: 30,
+          //     type: BottomNavigationBarType.fixed,
+          //     items: <BottomNavigationBarItem>[
+          //       BottomNavigationBarItem(
+          //         icon :Icon(Icons.home_outlined),
+          //         label: '',
+          //       ),
+          //       BottomNavigationBarItem(
+          //         icon:Icon(Icons.category_outlined),
+          //         label: '',
+          //       ),
+          //       BottomNavigationBarItem(
+          //         icon:Icon(Icons.add_rounded),
+          //         label: '',
+          //       ),
+          //       BottomNavigationBarItem(
+          //         icon:Icon(Icons.bookmark_outline_rounded),
+          //         label: '',
+          //       ),
+          //       BottomNavigationBarItem(
+          //         icon:Icon(Icons.settings_outlined),
+          //         label: '',
+          //       ),
+          //     ],
+          //     selectedItemColor: Colors.black,
+          //   ),
+          // ),
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
