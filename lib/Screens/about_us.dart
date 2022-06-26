5914f85f9fde135d5b2569c9c76f6ecc4db96b41
Shayan Kebriti <shayankebriti@gmail.com>
2022-06-26 14:22:19 +0430

    about us footer done

--- lib/Screens/about_us.dart
+++ lib/Screens/about_us.dart
@@ -33,135 +33,158 @@ class AboutUs extends StatelessWidget {
                   child: Container(
                     child: Container(
                       margin: EdgeInsets.all(10),
-                      child: Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Expanded(
-                                flex: 1,
-                                child: IconButton(
-                                  icon: Icon(Icons.arrow_back_ios,color: Colors.black),
-                                  onPressed: () => Navigator.of(context).pop(),
-                                ),
-                              ),
-                              Expanded(
-                                flex: 1,
-                                child: Container(
-                                  width: double.infinity,
-                                ),
-                              ),
-                              Text(
-                                "Developed by",
-                                style: TextStyle(fontSize: 25, color: Colors.black),
-                              ),
-                              Expanded(
-                                flex: 2,
-                                child: Container(
-                                  width: double.infinity,
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Divider(
-                            thickness: 2,
-                            color: Colors.black,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(30),
-                            child: Column(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                CircleAvatar(
-                                  backgroundImage:
-                                      AssetImage('assets/images/farbod.png'),
-                                  backgroundColor: Colors.white,
-                                  radius: 30,
+                                Expanded(
+                                  flex: 1,
+                                  child: IconButton(
+                                    icon: Icon(Icons.arrow_back_ios,color: Colors.black),
+                                    onPressed: () => Navigator.of(context).pop(),
+                                  ),
                                 ),
-                                SizedBox(
-                                  height: 5,
+                                Expanded(
+                                  flex: 1,
+                                  child: Container(
+                                    width: double.infinity,
+                                  ),
                                 ),
                                 Text(
-                                  "Farbod Saghafi",
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+                                  "Developed by",
+                                  style: TextStyle(fontSize: 25, color: Colors.black),
                                 ),
-                                SizedBox(height: 20,),
-                                ElevatedButton.icon(
-                                  onPressed: (){
-                                    _launchURL("https://github.com/itsfarbod");
-                                  },
-                                  icon: Image.asset("assets/images/logo/github.png", width: 25,),
-                                  label: Text('itsfarbod', style: TextStyle(color: Colors.white),),
-                                  style: ElevatedButton.styleFrom(
-                                    elevation: 0,
-                                    primary: Colors.black,
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(18.0),
+                                Expanded(
+                                  flex: 2,
+                                  child: Container(
+                                    width: double.infinity,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Divider(
+                              thickness: 2,
+                              color: Colors.black,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(20),
+                              child: Column(
+                                children: [
+                                  CircleAvatar(
+                                    backgroundImage:
+                                        AssetImage('assets/images/farbod.png'),
+                                    backgroundColor: Colors.white,
+                                    radius: 30,
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Text(
+                                    "Farbod Saghafi",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+                                  ),
+                                  SizedBox(height: 20,),
+                                  ElevatedButton.icon(
+                                    onPressed: (){
+                                      _launchURL("https://github.com/itsfarbod");
+                                    },
+                                    icon: Image.asset("assets/images/logo/github.png", width: 25,),
+                                    label: Text('itsfarbod', style: TextStyle(color: Colors.white),),
+                                    style: ElevatedButton.styleFrom(
+                                      elevation: 0,
+                                      primary: Colors.black,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(18.0),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(left: 45, right: 10),
+                                    child: Divider(
+                                      thickness: 1,
+                                      color: Colors.black87,
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  "AND",
+                                  style: TextStyle(
+                                    color: Colors.black87,
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(right: 45, left: 10),
+                                    child: Divider(
+                                      thickness: 1,
+                                      color: Colors.black87,
                                     ),
                                   ),
                                 ),
                               ],
                             ),
-                          ),
-                          Row(
-                            children: [
-                              Expanded(
-                                child: Padding(
-                                  padding: const EdgeInsets.only(left: 45, right: 10),
-                                  child: Divider(
-                                    thickness: 1,
-                                    color: Colors.black87,
-                                  ),
-                                ),
-                              ),
-                              Text(
-                                "AND",
-                                style: TextStyle(
-                                  color: Colors.black87,
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Expanded(
-                                child: Padding(
-                                  padding: const EdgeInsets.only(right: 45, left: 10),
-                                  child: Divider(
-                                    thickness: 1,
-                                    color: Colors.black87,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
 
-                          Padding(
-                            padding: const EdgeInsets.all(30),
-                            child: Column(
+                            Padding(
+                              padding: const EdgeInsets.all(20),
+                              child: Column(
+                                children: [
+                                  CircleAvatar(
+                                    backgroundImage:
+                                        AssetImage('assets/images/shayan.jpg'),
+                                    backgroundColor: Colors.white,
+                                    radius: 30,
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Text(
+                                    "Shayan Kebriti",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+                                  ),
+                                  SizedBox(height: 20,),
+                                  ElevatedButton.icon(
+                                    onPressed: (){
+                                      _launchURL("https://github.com/i4mShayan");
+                                    },
+                                    icon: Image.asset("assets/images/logo/github.png", width: 25,),
+                                    label: Text('i4mShayan', style: TextStyle(color: Colors.white),),
+                                    style: ElevatedButton.styleFrom(
+                                      elevation: 0,
+                                      primary: Colors.black,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(18.0),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(height: 10,),
+                            Divider(thickness: 2, color: Colors.black87,),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                               children: [
-                                CircleAvatar(
-                                  backgroundImage:
-                                      AssetImage('assets/images/shayan.jpg'),
-                                  backgroundColor: Colors.white,
-                                  radius: 30,
-                                ),
-                                SizedBox(
-                                  height: 5,
-                                ),
-                                Text(
-                                  "Shayan Kebriti",
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
-                                ),
-                                SizedBox(height: 20,),
                                 ElevatedButton.icon(
                                   onPressed: (){
-                                    _launchURL("https://github.com/i4mShayan");
+                                    _launchURL("https://github.com/i4mShayan/Peddit");
                                   },
                                   icon: Image.asset("assets/images/logo/github.png", width: 25,),
-                                  label: Text('i4mShayan', style: TextStyle(color: Colors.white),),
+                                  label: Text('Project repository', style: TextStyle(color: Colors.white),),
                                   style: ElevatedButton.styleFrom(
                                     elevation: 0,
                                     primary: Colors.black,
@@ -170,12 +193,11 @@ class AboutUs extends StatelessWidget {
                                     ),
                                   ),
                                 ),
+                                Text("AP Spring 1401", style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),),
                               ],
                             ),
-                          ),
-                          SizedBox(height: 10,),
-                          Divider(thickness: 2, color: Colors.black87,),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                     decoration: BoxDecoration(
@@ -185,7 +207,8 @@ class AboutUs extends StatelessWidget {
                 ),
               ),
             ),
-          )),
+          )
+      ),
     );
   }
 }
