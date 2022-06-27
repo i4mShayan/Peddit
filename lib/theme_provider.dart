ce9cb77648013af2b809362fe4ac419f8fb8eb29
Shayan Kebriti <shayankebriti@gmail.com>
2022-06-27 19:41:38 +0430

    initial themeMode fixed

--- lib/theme_provider.dart
+++ lib/theme_provider.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/scheduler.dart';
 import 'libs.dart';
 
 class ThemeProvider extends ChangeNotifier{
-  ThemeMode themeMode = ThemeMode.system;
+  ThemeMode themeMode = SchedulerBinding.instance.window.platformBrightness==Brightness.dark ? ThemeMode.dark:ThemeMode.light;
 
   bool get isDarkMode => themeMode==ThemeMode.dark;
 
